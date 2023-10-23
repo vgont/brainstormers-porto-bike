e13ebeb50f2d1cab8f48dc549c65702f208731b4
vgont <vgontijo34@gmail.com>
2023-10-23 12:00:39 -0300

    home page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 import Button from "./components/Button";
+import Header from "./components/Header";
+import PortoImage from "./components/PortoImage";
 
 export default function Home() {
   return (
-    <main>
-      <Button isBlueColor>Teste</Button>
+    <main className="flex flex-col justify-center items-center">
+      <Header />
+      <PortoImage />
+      <div className="mt-48 flex flex-col gap-10">
+        <Link href="/tipo">
+          <Button isBlueColor>Cadastrar bikes</Button>
+        </Link>
+        <Link href="/bikes">
+          <Button>Visualizar bikes</Button>
+        </Link>
+      </div>
     </main>
   );
 }
