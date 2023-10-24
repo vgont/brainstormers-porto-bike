68f33243967924f9c3b6c60d6b8bd68e6eeb0647
vgont <vgontijo34@gmail.com>
2023-10-23 22:34:16 -0300

    DocumentosBike page modified

--- src/app/documentos/page.tsx
+++ src/app/documentos/page.tsx
@@ -5,19 +5,16 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Title from "../components/Title";
 
-export default function ModificacoesBike() {
+export default function DocumentosBike() {
   return (
     <main className="flex flex-col justify-center items-center">
       <Header />
       <Title>{"Anexe os seguintes documentos da bike:"}</Title>
       <div className="mt-20 flex flex-col gap-28">
-        <FileUploader idName="nota" isBlueColor>
-          {"Nota fiscal"}
-        </FileUploader>
-        <FileUploader idName="numSerie" isBlueColor>
-          {"N.º de série"}
-        </FileUploader>
+        <FileUploader idName="nota">{"Nota fiscal"}</FileUploader>
+        <FileUploader idName="numSerie">{"N.º de série"}</FileUploader>
       </div>
+
       <Footer pathUrlBack="/modificacoes" pathUrlNext="/" />
     </main>
   );
