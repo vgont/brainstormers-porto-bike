3c501808cfea32a03d2a319a3fbe2bf09a3fbcce
vgont <vgontijo34@gmail.com>
2023-10-24 15:09:30 -0300

    text-black added

--- src/app/components/SubTitle.tsx
+++ src/app/components/SubTitle.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 const SubTitle: React.FC<PropsWithChildren> = ({ children }) => {
-  return <h1 className="text-base font-medium">{children}</h1>;
+  return <h1 className="text-base font-medium text-black">{children}</h1>;
 };
 
 export default SubTitle;
