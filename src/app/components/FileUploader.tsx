840558ee6e73cf8ce84272744dfa1de28dade182
vgont <vgontijo34@gmail.com>
2023-10-23 22:33:54 -0300

    new components added

--- src/app/components/FileUploader.tsx
+++ src/app/components/FileUploader.tsx
@@ -1,29 +1,43 @@
 import Image from "next/image";
 import { PropsWithChildren, useState } from "react";
-import { ImCheckmark2, ImCheckmark } from "react-icons/im";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { ImCheckmark } from "react-icons/im";
+import Modal from "./Modal";
 
 interface IFileUploaderProps {
   idName: string;
-  isBlueColor?: boolean;
 }
 
 const FileUploader: React.FC<IFileUploaderProps & PropsWithChildren> = ({
   children,
-  isBlueColor,
   idName,
 }) => {
   const [isUploaded, setIsUploaded] = useState<boolean>(false);
+
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0)
       return setIsUploaded(true);
     if (event.target.files && event.target.files.length === 0)
       return setIsUploaded(false);
   };
+
+  const [isInfoRead, setIsInfoRead] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleInfoClick = () => {
+    setIsInfoRead(true);
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="flex flex-row justify-center items-center gap-5">
+      <button className="text-blue-500 text-lg" onClick={handleInfoClick}>
+        {BsFillInfoCircleFill()}
+      </button>
       <label
         htmlFor={idName}
-        className={`flex flex-row h-14 p-5 justify-center items-center rounded-lg font-semibold w-full gap-5 bg-blue-500 text-white`}
+        className={`flex flex-row h-14 p-5 justify-center items-center rounded-lg font-semibold w-full gap-5
+        ${isInfoRead ? "bg-blue-500 text-white" : "bg-blue-300 text-gray-400"}`}
       >
         {children}
         <Image
@@ -34,10 +48,17 @@ const FileUploader: React.FC<IFileUploaderProps & PropsWithChildren> = ({
           height={31}
         />
       </label>
-      <input type="file" id={idName} hidden onChange={handleFileUpload} />
+      <input
+        type="file"
+        id={idName}
+        hidden
+        onChange={handleFileUpload}
+        disabled={isInfoRead ? false : true}
+      />
       <span className="text-blue-600 text-2xl">
-        {isUploaded ? ImCheckmark() : ImCheckmark2()}
+        {isUploaded ? ImCheckmark() : null}
       </span>
+      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </div>
   );
 };
--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,7 +1,21 @@
 interface IModal {
-  isOpen: boolean;
+  isModalOpen: boolean;
+  setIsModalOpen: (isOpen: false) => void;
 }
 
-const Modal: React.FC<IModal> = ({ isOpen }) => {
-  if (isOpen) return <div></div>;
+const Modal: React.FC<IModal> = ({ isModalOpen, setIsModalOpen }) => {
+  if (isModalOpen)
+    return (
+      <div className="fixed inset-10 flex items-center justify-center z-50 w-3/4 h-2/3 m-auto bg-gray-400 rounded text-white">
+        <button
+          className="text-black font-extrabold self-start mr-auto ml-2"
+          onClick={() => setIsModalOpen(false)}
+        >
+          X
+        </button>
+      </div>
+    );
+  return null;
 };
+
+export default Modal;
