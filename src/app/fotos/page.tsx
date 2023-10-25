017e1d1a59c243ef8028eb32e128c4c7a8216c7a
vgont <vgontijo34@gmail.com>
2023-10-25 18:58:05 -0300

    FotosBike page created

---
+++ src/app/fotos/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import FileUploader from "../components/FileUploader";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Title from "../components/Title";
+
+export default function FotosBike() {
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <Header />
+      <Title>{"Anexe as seguintes fotos da bike:"}</Title>
+      <div className="mt-20 flex flex-col gap-28">
+        <FileUploader idName="bikeLeft" pathImageExample="/images/bikeLeft.jpg">
+          {"Lado esquerdo"}
+        </FileUploader>
+        <FileUploader
+          idName="bikeRight"
+          pathImageExample="/images/bikeRight.jpg"
+        >
+          {"Lado direito"}
+        </FileUploader>
+      </div>
+      <Footer pathUrlBack="/documentos" pathUrlNext="/fotos" />
+    </main>
+  );
+}
