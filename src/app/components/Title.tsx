8e2521c4eca72e0484c1aaf7acd15aeb5817b735
vgont <vgontijo34@gmail.com>
2023-10-30 22:29:26 -0300

    style fixed

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const Title: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <h1 className="text-lg font-semibold mt-28 w-1/2 text-center">
+    <h1 className="text-lg font-semibold mt-16 w-1/2 text-center">
       {children}
     </h1>
   );
--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import React from "react";
 
-interface IToggle {
-  onclick: () => void;
+export interface IToggle {
+  isToggled: boolean;
+  setIsToggled: (isToggled: boolean) => void;
 }
 
-const Toggle: React.FC<IToggle> = ({ onclick }) => {
-  const [isToggled, setIsToggled] = useState<boolean>(false);
-
-  const handleOnClick = () => {
-    onclick();
-    setIsToggled(!isToggled);
-  };
+const Toggle: React.FC<IToggle> = ({ isToggled, setIsToggled }) => {
   return (
     <label className="relative inline-flex items-center mb-5 cursor-pointer">
       <input
-        onClick={handleOnClick}
         type="checkbox"
-        value=""
-        className="sr-only peer"
+        className="sr-only"
+        checked={isToggled}
+        onChange={() => setIsToggled(!isToggled)}
       />
-      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      <div
+        className={`w-11 h-6 ${
+          isToggled ? "bg-blue-500" : "bg-gray-200"
+        } rounded-full ml-3 text-sm font-medium peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 relative`}
+      >
+        <div
+          className={`absolute w-6 h-6 ${
+            isToggled ? "bg-gray-100" : "bg-blue-500"
+          } rounded-full transition-transform transform left-0 border border-blue-700 ${
+            isToggled ? "translate-x-full" : "translate-x-0"
+          }`}
+        ></div>
+      </div>
       <span className="ml-3 text-sm font-medium">
         {isToggled ? "Sim" : "Não"}
       </span>
