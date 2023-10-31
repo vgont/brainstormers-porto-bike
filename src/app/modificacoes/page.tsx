47503849eb0cd8b3bdcb20a780d4a245a330d8af
vgont <vgontijo34@gmail.com>
2023-10-30 22:32:03 -0300

    props added

--- src/app/modificacoes/page.tsx
+++ src/app/modificacoes/page.tsx
@@ -4,27 +4,29 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Title from "../components/Title";
 import BikeComplement from "../components/BikeComplement";
-import useBikeStore from "../stores/useBikeStore";
+import { useState } from "react";
 
 export default function ModificacoesBike() {
-  const { isModified, setIsModified } = useBikeStore();
-  const { haveAcessory, setHaveAcessory } = useBikeStore();
+  const [isModified, setIsModified] = useState(false);
+  const [haveAcessory, setHaveAcessory] = useState(false);
 
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
+    <main className="flex flex-col items-center h-screen">
       <Header />
       <Title>{"Sua bike possui"}</Title>
-      <div className="flex flex-col space-x-2">
+      <div className="flex flex-col space-x-2 mb-auto">
         <BikeComplement
           complement="Modificações"
-          onclick={() => setIsModified(!isModified)}
+          isToggled={isModified}
+          setIsToggled={setIsModified}
         />
         <BikeComplement
           complement="Acessórios"
-          onclick={() => setHaveAcessory(!haveAcessory)}
+          isToggled={haveAcessory}
+          setIsToggled={setHaveAcessory}
         />
       </div>
-      <Footer pathUrlBack="/tipo" pathUrlNext="/documentos" />
+      <Footer pathUrlBack="/eletrica" pathUrlNext="/documentos" isValidated />
     </main>
   );
 }
